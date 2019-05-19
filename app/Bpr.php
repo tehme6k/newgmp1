4426af39b2556f4bd2b200651c444d863d250f91
tehme6k <moshimoshi6k@gmail.com>
2019-05-19 01:40:09 -0500

    4th commit

---
+++ app/Bpr.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bpr extends Model
+{
+    protected $guarded = [];
+}
---
+++ database/migrations/2019_05_19_063455_create_mpr_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMprProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mpr_product', function (Blueprint $table) {
+            $table->integer('mpr_id');
+            $table->integer('product_id');
+            $table->double('amount', 9, 2)->unsigned();
+            $table->integer('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mpr_product');
+    }
+}
