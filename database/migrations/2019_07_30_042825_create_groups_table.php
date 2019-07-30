d057fbd568c2c0292a9cb33f365aa8edc74729e0
tehme6k <moshimoshi6k@gmail.com>
2019-07-29 23:38:18 -0500

    group and pivot table created

---
+++ database/migrations/2019_07_30_042825_create_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('version');
+            $table->bigInteger('country_id');
+            $table->bigInteger('type_id');
+            $table->integer('serving_size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
